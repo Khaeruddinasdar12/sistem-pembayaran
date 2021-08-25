30575ce01513e817e11634114e7cd9c686e0d1fa
Khaeruddin Asdar <46445417+Khaeruddinasdar12@users.noreply.github.com>
2021-08-25 22:27:28 +0800

    perbaiki seeder User

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -7,10 +7,17 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('admins')->insert([
-            'name' => 'Admin Super',
-            'email' => 'admin@gmail.com',
+            'name' => 'Khaeruddin Asdar',
+            'email' => 'khaeruddinasdar12@gmail.com',
             'password' => bcrypt(12345678),
             'status' => '0', //admin
+        ]);
+
+        DB::table('admins')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt(12345678),
+            'status' => '1', //penagih
         ]);
 
         DB::table('admins')->insert([
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -7,15 +7,22 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Mansyur',
-            'email' => 'mansyur@gmail.com',
+            'name' => 'Ramang',
+            'email' => 'ramang@gmail.com',
             'password' => bcrypt(12345678),
             'alamat' => 'rt/rw 001/002', 
         ]);
 
         DB::table('users')->insert([
-            'name' => 'Rispo',
-            'email' => 'rispo@gmail.com',
+            'name' => 'Anwar',
+            'email' => 'anwar@gmail.com',
+            'password' => bcrypt(12345678),
+            'alamat' => 'rt/rw 003/006',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Dg. Pajja',
+            'email' => 'pajja@gmail.com',
             'password' => bcrypt(12345678),
             'alamat' => 'rt/rw 003/006',
         ]);
